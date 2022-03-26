acd69808d5f68838fc59a8ee8f28748712a1ab2c
Felipe Contreras <fecontreras@interside.cl>
2022-03-26 11:40:50 -0300

    change driver

--- src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
@@ -84,8 +84,8 @@ class DevopsApplicationTests {
 	  System.out.print("**********************Ejecutando Test de Selenium***********************");
 	  JavascriptExecutor js;
 
-	  // System.setProperty("webdriver.chrome.driver","src/driver/linx/chromedriver"); // Linux
-	  System.setProperty("webdriver.chrome.driver","src/driver/win/chromedriver.exe"); // Windows
+	  /System.setProperty("webdriver.chrome.driver","src/driver/linx/chromedriver"); // Linux
+	  // System.setProperty("webdriver.chrome.driver","src/driver/win/chromedriver.exe"); // Windows
 	  // driver = new FirefoxDriver(); // Firefox
 	  // File pathBinary = new File("/usr/bin/google-chrome");
 	  // System.setProperty("webdriver.chrome.drive", pathBinary.getAbsolutePath());
